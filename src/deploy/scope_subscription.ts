0c19b81ce437596bffeda5c9a06178d6da7f84ad
Deepak Dahiya <59823596+t-dedah@users.noreply.github.com>
2021-06-17 17:21:23 +0000

    Improved error message

--- src/deploy/scope_subscription.ts
+++ src/deploy/scope_subscription.ts
@@ -69,7 +69,7 @@ export async function DeploySubscriptionScope(azPath: string, region: string, te
         var deploymentCode = await exec(`"${azPath}" deployment sub create ${azDeployParameters} -o json`, [], deployOptions);
         if (commandStdErr.trim().length !== 0) {
             core.error(commandStdErr)
-            throw new Error("Throwing error as stderr is not null: " + commandStdErr)
+            throw new Error(`Deployment process failed as some lines were written to stderr: ${commandStdErr}`)
         } else {
             if (deploymentCode != 0) {
                 core.error("Deployment failed.")
